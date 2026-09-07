76b505ca34ef31371f4ec4bf1cb9b870bdd1256a
SPaCE69 <rahulnishad6969@gmail.com>
2026-09-07 23:06:34 +0530

    need to deploy plus 3rd party services setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,20 @@
-import express from "express"
+import express from "express";
+import path from "path";
+import { ENV } from "./config/ENV.js";
 const app = express();
 
+const __dirname = path.resolve();
 
 const PORT = 3000;
 
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
 app.listen(PORT, () => {
-    console.log("Server Listening at PORT:", PORT)
-})
+  console.log("Server Listening at PORT:", PORT);
+});
