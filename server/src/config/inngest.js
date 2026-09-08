e1f4df364e8575251a737bafd5ef5c229ef97ac3
SPaCE69 <rahulnishad6969@gmail.com>
2026-09-08 12:37:59 +0530

    deployment on render for inngest

---
+++ server/src/config/inngest.js
@@ -0,0 +1,45 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
+
+export const inngest = new Inngest({ id: "space-buyz" });
+
+const syncUser = inngest.createFunction(
+  {
+    id: "sync-user",
+    triggers: {
+      event: "clerk/user.created",
+    },
+  },
+  async ({ event }) => {
+    await connectDB();
+    const { id, first_name, last_name, image_url, email_addresses } =
+      event.data;
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""} ` || "User",
+      imageURL: image_url,
+      addresses: [],
+      wishlist: [],
+    };
+    await User.create(newUser);
+  },
+);
+
+const deleteUserFromDB = inngest.createFunction(
+  {
+    id: "delete-user-from-db",
+    triggers: {
+      event: "clerk/user.deleted",
+    },
+  },
+
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  },
+);
+
+export const functions = [syncUser, deleteUserFromDB];
--- server/src/server.js
+++ server/src/server.js
@@ -3,13 +3,17 @@ import path from "path";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { functions, inngest } from "./config/inngest.js";
+import { serve } from "inngest/express";
+
 const app = express();
 
 const __dirname = path.resolve();
 
 const PORT = 3000;
-
+app.use(express.json());
 app.use(clerkMiddleware());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
